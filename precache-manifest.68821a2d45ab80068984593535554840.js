3173a37fb39686f82a08642252fd7500d3066a21
Honex75 <honex75@Honex75s-MacBook-Pro.local>
2020-05-11 15:23:25 +0300

    Updates

---
+++ precache-manifest.68821a2d45ab80068984593535554840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedb0f8d8e8fb801f3f0dd42ecf86cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9973bdddb3e46744095c",
+    "url": "/static/css/main.30a31894.chunk.css"
+  },
+  {
+    "revision": "15ea9ea3d2d29b17f012",
+    "url": "/static/js/2.4e0b125a.chunk.js"
+  },
+  {
+    "revision": "43c90780224d05185bec687a556a8de7",
+    "url": "/static/js/2.4e0b125a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9973bdddb3e46744095c",
+    "url": "/static/js/main.2441177a.chunk.js"
+  },
+  {
+    "revision": "e3c6314b912b787be61d",
+    "url": "/static/js/runtime-main.f5d66d6e.js"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/static/media/google.09aea0f5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c42ff060af08a71dfbbdb973304c6073.js"
+  "/precache-manifest.68821a2d45ab80068984593535554840.js"
 );
 
 self.addEventListener('message', (event) => {
